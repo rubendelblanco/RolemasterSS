e7fd4cd6dd5ab3013894041dd1dd19481f46c2e2
Ruben del Blanco <rubendelblanco@gmail.com>
2024-08-25 23:30:27 +0200

    mierdolo puro. Seguimos

--- module/sheets/items/rmss_profession_sheet.js
+++ module/sheets/items/rmss_profession_sheet.js
@@ -1,12 +1,14 @@
 export default class RMSSProfessionSheet extends ItemSheet {
+
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
             width: 620,
             height: 640,
             template: "systems/rmss/templates/sheets/professions/rmss-profession-sheet.html",
-            classes: ["rmss", "sheet", "profession"]
+            classes: ["rmss", "sheet", "profession"],
         });
     }
+
 
     // If our sheet is called here it is.
     get template() {
@@ -16,8 +18,6 @@ export default class RMSSProfessionSheet extends ItemSheet {
     async getData() {
         const baseData = await super.getData();
         const context = super.getData();
-
-        //let enrichedDescription = await TextEditor.enrichHTML(this.item.system.description, {async: true});
 
         let sheetData = {
             owner: this.item.isOwner,
@@ -32,65 +32,6 @@ export default class RMSSProfessionSheet extends ItemSheet {
         return sheetData;
     }
 
-    async _updateObject(event, formData) {
-        console.log("PROFESSION SKILL CATEGORIES");
-        for (const item of this.game.items) {
-            if (item.type === "skill_category") {
-                item.delete();
-            }
-        }
-        /*
-        const pack = game.packs.get(formData.selectOptions);
-        const skillCategoryData = await pack.getIndex();
-
-        console.log("Importing New Skill Categories.");
-
-        for (const sc of skillCategoryData) {
-            const newitem = await pack.getDocument(sc._id);
-
-            let newDocuments = [];
-            if (newitem.type === "skill_category") {
-                console.log(newitem);
-                newDocuments.push(newitem);
-            }
-            if (newDocuments.length > 0) {
-                await Item.createDocuments(newDocuments, {parent: this.character});
-            }
-        } */
-    }
-
-    activateListeners(html) {
-        // Show Sheet Settings
-        html.find(".import-skillcats").click(async ev => {
-            let skillCategoriesPack = null;
-            this.object.system.skills_category = [];
-            this.object.update();
-
-            for (const pack of game.packs) {
-                if (pack.metadata.name === "skill_categories") {
-                    skillCategoriesPack = pack;
-                    break;
-                }
-            }
-
-            for (let i of skillCategoriesPack.index) {
-                this.object.system.skills_category.push({"name":i.name, "progression":""});
-            }
-
-            this.object.system.skills_category.sort(function(a, b) {
-                if (a.name < b.name) {
-                    return -1;
-                }
-                if (a.name > b.name) {
-                    return 1;
-                }
-                return 0;
-            });
-
-           this.object.update();
-
-        });
-    }
 
     _prepareItems(context){
         console.log(context);
